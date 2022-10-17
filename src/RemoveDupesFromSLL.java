4425c2c7ed8a407384ca9717d4a2dd56545b3054
ichbinjay <bvred926@gmail.com>
2022-10-17 17:23:49 +0530

    first commit

---
+++ src/RemoveDupesFromSLL.java
@@ -0,0 +1,73 @@
+class SinglyLinkedList{
+    int data;
+    SinglyLinkedList next;
+    SinglyLinkedList head = null;
+
+    public void add(int data){
+        SinglyLinkedList newNode = new SinglyLinkedList();
+        newNode.data = data;
+        newNode.next = null;
+
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            SinglyLinkedList temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void print(){
+        SinglyLinkedList temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void sort(){
+        SinglyLinkedList temp = head;
+        while(temp != null){
+            SinglyLinkedList temp2 = temp.next;
+            while(temp2 != null){
+                if(temp.data > temp2.data){
+                    int tempData = temp.data;
+                    temp.data = temp2.data;
+                    temp2.data = tempData;
+                }
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void removeDuplicates(){
+        SinglyLinkedList ptr = head;
+        while(ptr.next != null){
+            if(ptr.data == ptr.next.data) ptr.next = ptr.next.next;
+            else ptr = ptr.next;
+        }
+    }
+}
+public class RemoveDupesFromSLL {
+    public static void main(String[]args){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.add(2);
+        sll.add(3);
+        sll.add(4);
+        sll.add(1);
+        sll.add(4);
+        sll.add(5);
+        sll.add(5);
+        
+        sll.print();    
+        sll.sort();
+        sll.print();
+        sll.removeDuplicates();
+        sll.print();
+    }
+}
---
+++ src/performXOROnTwoBinaryStrings.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class performXOROnTwoBinaryStrings {
+    public static void main(String[] args) throws Exception {
+        //I M16 - Exclusive Or
+
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        
+        String res = "";
+        int minLen = Math.min(s1.length(), s2.length());
+
+        String excessZeros = "";
+        for (int i = 0; i < Math.abs(s1.length() - s2.length()); i++) {
+            excessZeros += "0";
+        }
+        if(s1.length() > s2.length()) {
+            s2 = excessZeros + s2;
+        } else {
+            s1 = excessZeros + s1;
+        }
+
+        minLen = Math.min(s1.length(), s2.length());
+        for(int i = minLen-1; i >= 0; i--){
+            if(s1.charAt(i) != s2.charAt(i)){
+                res = "1" + res;
+            }
+            else{
+                res = "0" + res;
+            }
+        }
+        System.out.println(res);
+    }
+}
