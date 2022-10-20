f234e017eb2cc39bfe15d53c55b0b183b634dd32
ichbinjay <bvred926@gmail.com>
2022-10-20 19:34:00 +0530

    lol

--- src/Subarrayrem.java
+++ src/Subarrayrem.java
@@ -1,21 +1,16 @@
-import java.io.*;
-import java.util.*;
- 
 class Subarrayrem {
     // Driver Code
     public static void main(String[] args)
     {
         int[] a = { 1, 0, 0, 0, 1};
-        System.out.println("Maximum contiguous sum is "
-                           + maxSubArraySum(a));
+        System.out.println("Maximum contiguous sum is " + maxSubArraySum(a));
     }
  
     // Function Call
     static int maxSubArraySum(int a[])
     {
         int size = a.length;
-        int max_so_far = Integer.MIN_VALUE, max_ending_here
-                                            = 0;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
  
         for (int i = 0; i < size; i++) {
             max_ending_here = max_ending_here ^ a[i];
