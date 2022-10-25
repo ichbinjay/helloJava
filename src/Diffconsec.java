b038dec23b6aa36dbdf2f1c3f14bed9c12602601
ichbinjay <bvred926@gmail.com>
2022-10-25 17:19:29 +0530

    dsf

--- src/Diffconsec.java
+++ src/Diffconsec.java
@@ -1,19 +1,50 @@
 import java.util.Scanner;
 
 public class Diffconsec {
+    public static boolean vowelcheck(char w, char x) {
+        //vowels = {a, e, i, o, u, y}
+        if(w == 'a'){
+            if(x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'y'){
+                return true;
+            }
+        }
+        else if(w == 'e'){
+            if(x == 'a' || x == 'i' || x == 'o' || x == 'u' || x == 'y'){
+                return true;
+            }
+        }
+        else if(w == 'i'){
+            if(x == 'a' || x == 'e' || x == 'o' || x == 'u' || x == 'y'){
+                return true;
+            }
+        }
+        else if(w == 'o'){
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'u' || x == 'y'){
+                return true;
+            }
+        }
+        else if(w == 'u'){
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'y'){
+                return true;
+            }
+        }
+        else if(w == 'y'){
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while(n-- > 0){
-            int length = sc.nextInt();
             String s = sc.next();
-            System.out.println(s);
-            int similarChars = 0;
-            for(int i=0; i<length-1; i++){
-                if(s.charAt(i) == s.charAt(i+1)) similarChars++;
+            for (int i = 0; i < s.length() - 1; i++) {
+                if (s.charAt(i) == s.charAt(i + 1) || vowelcheck(s.charAt((i)), s.charAt(i + 1))) {
+                    System.out.println("No");
+                    System.exit(0);
+                }
             }
-            System.out.println(similarChars);
-        }
+            System.out.println("Yes");
         sc.close();
     }
 }
