2e99664998508f1c45fb9e5ce6d6121076179420
ichbinjay <bvred926@gmail.com>
2023-01-14 20:54:07 +0530

    hmmok

---
+++ src/validParentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else if(ch == ')') {
+                if(st.size() == 0||st.peek() != '(') return false;
+                else st.pop();
+            }
+            else if(ch == '}') {
+                if(st.size() == 0||st.peek() != '{') return false;
+                else st.pop();
+            }
+            else if(ch == ']') {
+                if(st.size() == 0||st.peek() != '[') return false;
+                else st.pop();
+            }
+        }
+        if(st.size() == 0) return true;
+        else return false;
+    }
+}
