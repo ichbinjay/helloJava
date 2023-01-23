56cfd029f031b4a800add9e944a6e98f24061aa3
ichbinjay <bvred926@gmail.com>
2023-01-23 20:54:19 +0530

    hii

---
+++ src/topKFrequentElements.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        from collections import defaultdict
+        ht = defaultdict(lambda: 1)
+        for num in nums:
+            ht[num] += 1
+        ht = sorted(ht.items(), key=lambda x: x[1], reverse=True)
+        return [x[0] for x in ht[:k]]
