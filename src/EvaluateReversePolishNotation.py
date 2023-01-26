dd091db28dfd7ccb80d6ba9e9c524dfa6ec994e2
ichbinjay <bvred926@gmail.com>
2023-01-26 20:54:33 +0530

    gn

--- src/EvaluateReversePolishNotation.py
+++ src/EvaluateReversePolishNotation.py
@@ -1,22 +1,28 @@
+import math
+
 class Solution(object):
     def evalRPN(self, tokens):
         stack = []
         for i in tokens:
-            try 
-                stack.append(i)
-            else:
+            try:
+                int(i)
+                stack.append(int(i))
+            except ValueError:
                 a = stack[-2]
                 b = stack[-1]
                 res = None
                 if i == "+":
-                    res = a+b
-                elif i=="-":
-                    res = a-b
-                elif i=="*":
-                    res = a*b
+                    res = a + b
+                elif i == "-":
+                    res = a - b
+                elif i == "*":
+                    res = a *  b
                 else:
-                    res = a/b
-                stack[-2] = res
-                del stack[-1]
+                    res = math.trunc(a / b)
+                del stack[-2:]
+                stack.append(res)
         return int(stack[-1])
-                
+
+
+sol = Solution()
+print(sol.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
