eea9a8148580a38fe4d6925454c76a59519ab663
ichbinjay <bvred926@gmail.com>
2023-01-26 20:53:16 +0530

    gdn8

---
+++ src/EnocodeAndDecodeStrings.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class EncodeAndDecodeStrings {
+    public static String encode(String[] strs){
+        StringBuilder res = new StringBuilder();
+        for(String str : strs){
+            res.append(str.length()).append("#").append(str);
+        }
+        return res.toString();
+    }
+
+    public static String[] decode(String str){
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while(i < str.length()){
+            int j = str.indexOf('#', i);
+            int len = Integer.parseInt(str.substring(i, j));
+            res.add(str.substring(j+1, j+1+len));
+            i = j+1+len;
+        }
+        return res.toArray(new String[0]);
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        EncodeAndDecodeStrings codec = new EncodeAndDecodeStrings();
+        System.out.println(Arrays.toString(EncodeAndDecodeStrings.decode(EncodeAndDecodeStrings.encode(new String[]{"Hello", "World"}))));
+    }
+}
---
+++ src/minStack.py
@@ -0,0 +1,37 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = list()
+        self.minstack = list()
+
+    def push(self, val):
+        self.stack.append(val)
+        
+        if self.minstack:
+            if self.minstack[-1] < val:
+                self.minstack.append(self.minstack[-1])
+            else:
+                self.minstack.append(val)
+        else:
+            self.minstack.append(val)
+
+    def pop(self):
+        del self.stack[-1]
+        del self.minstack[-1]
+        
+
+    def top(self):
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        return self.minstack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
