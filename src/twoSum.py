9c5bdd132136cd09eab6e8e0d161351070360e37
ichbinjay <bvred926@gmail.com>
2023-02-18 21:04:53 +0530

    fsdz

--- src/twoSum.py
+++ src/twoSum.py
@@ -1,3 +1,4 @@
+# approach: hash map
 nums = [2, 7, 11, 5]
 target = 9
 hm = dict()
@@ -7,3 +8,20 @@ for i, num in enumerate(nums):
         print([hm[target - num], i])
         break
     hm[num] = i
+
+# approach: two pointers
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            if numbers[l]+numbers[r] > target:
+                r-=1
+            elif numbers[l]+numbers[r]< target:
+                l-=1
+            elif numbers[l]+numbers[r]==target:
+                return [l+1, r+1]
+
+sol = Solution()
+print(sol.twoSum([2,7,11,15],9))
