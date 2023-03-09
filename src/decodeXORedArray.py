8253e8262ccfd803d5b911e459e005cc17f97830
ichbinjay <bvred926@gmail.com>
2023-03-09 18:52:16 +0530

    srg

---
+++ src/decodeXORedArray.py
@@ -0,0 +1,6 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        decoded = [first]
+        for i in range(len(encoded)):
+            decoded.append(encoded[i] ^ decoded[i])
+        return decoded
