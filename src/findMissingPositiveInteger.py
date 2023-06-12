4e8debd7615a73b19a834b928c5d01e623cc6a41
ichbinjay <80576787+ichbinjay@users.noreply.github.com>
2023-06-12 06:32:36 +0000

    jdfg

---
+++ src/findMissingPositiveInteger.py
@@ -0,0 +1,20 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    for i in range(n):
+        if arr[i] <= 0 or arr[i] > n:
+            arr[i] = n+10
+    
+    for i in range(n):
+        if abs(arr[i])!=n+10:
+            arr[abs(arr[i])-1] = -abs(arr[abs(arr[i])-1])
+
+    for i in range(n):
+        if arr[i] > 0:
+            print(i+1)
+            break
+    else:
+        print(n+1)
+        
