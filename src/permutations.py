ccbfa22af13f3af586940418fbfd1c17f1ffe343
ichbinjay <80576787+ichbinjay@users.noreply.github.com>
2023-06-30 10:55:41 +0000

    afsd

--- src/permutations.py
+++ src/permutations.py
@@ -18,3 +18,37 @@ class Solution:
 
 s = Solution()
 print(s.permute([2, 3, 6]))
+
+# other
+def permuteUnique(nums):
+    nums.sort()  # Sort the array to handle duplicates efficiently
+    result = []
+    visited = [False] * len(nums)
+
+    def backtrack(curr_perm, visited, depth):
+        if depth == len(nums):
+            result.append(curr_perm)
+            return
+
+        for i in range(len(nums)):
+            if visited[i]:
+                continue
+
+            if i > 0 and nums[i] == nums[i-1] and not visited[i-1]:
+                continue
+
+            visited[i] = True
+            backtrack(curr_perm + [nums[i]], visited, depth + 1)
+            visited[i] = False
+
+    backtrack([], visited, 0)
+    return result
+
+# Read input
+N = int(input())
+nums = list(map(int, input().split()))
+
+# Call the function and print the result
+permutations = permuteUnique(nums)
+for perm in permutations:
+    print(*perm)
